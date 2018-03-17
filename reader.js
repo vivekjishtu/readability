c6cf5cfd504b2e5a661d02e48bdbee5fa07fa769
Vivek Jishtu <github@vivekjishtu.com>
2018-03-17 22:02:51 +0530

    Create reader.js

---
+++ reader.js
@@ -0,0 +1,6 @@
+var my_awesome_script = document.createElement('script');
+my_awesome_script.setAttribute('src','https://vivekjishtu.github.io/readability/Readability.js');
+my_awesome_script.onload = function() { 
+  //Loaded 
+}
+document.head.appendChild(my_awesome_script);
